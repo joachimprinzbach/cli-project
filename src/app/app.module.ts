abcd49aff9c77a54467ac5acf9bf746bc32bb042
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2017-07-09 17:55:33 +0200

    add sample for ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,52 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
+import {Http, HttpModule} from "@angular/http";
 
 import {AppComponent} from "./app.component";
-import {SampleModule} from "angular-library";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import {RouterModule, Routes} from "@angular/router";
+
+export function HttpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    SampleModule
-
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
